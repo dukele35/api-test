7fa9f53fd5c2adb426b2023780d30332aef80586
Duc Le <dukele35@gmail.com>
2022-08-26 17:29:32 +0700

    tidying up

--- dbl/app/serializers.py
+++ dbl/app/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['id', 'username', 'email']
--- dbl/app/views.py
+++ dbl/app/views.py
@@ -3,9 +3,6 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from dbl.app.serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
+    queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
