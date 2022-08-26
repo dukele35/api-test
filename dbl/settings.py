c6740a9bd6959c6592dc2f84d21fb5bee0b41a45
Duc Le <dukele35@gmail.com>
2022-08-26 15:40:27 +0700

    remove pagination

--- dbl/settings.py
+++ dbl/settings.py
@@ -136,8 +136,6 @@ STATIC_ROOT = os.getcwd() + "/static/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
