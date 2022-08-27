bd97db14c99aa748f4e11afe0f3e63cb66233551
Duc Le <dukele35@gmail.com>
2022-08-28 03:02:48 +0700

    first test

--- dblapp/tests.py
+++ dblapp/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from model_bakery import baker
+from dblapp.models import FactoryRecord
+from pprint import pprint
 
-# Create your tests here.
+class FactoryRecordTestCase(TestCase):
+    def setUp(self):
+        self.factory = baker.make('FactoryRecord')
+        # pprint(self.factory.__dict__)
+
+    def test_model_str(self):
+        factory = FactoryRecord.objects.create(factory_name="Org1")
+        self.assertIs(type(str(factory)), str)
+
